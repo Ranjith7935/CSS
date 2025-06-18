b6a0f697b5e4dbcfdae5875544f0b3138f316a04
Ranjith7935 <studetn@apnacollege.in>
2025-06-18 23:50:26 +0530

    css oneshot

---
+++ CSSoneshot/understanding css - styling ✔️.jl
@@ -0,0 +1,16 @@
+understanding css - styling ✔️
+making css file ✔️
+understanding linking ✔️
+css boilerplare ✔️
+understanding css writing way✔️
+understanding width ,height✔️
+understanding units,px and % ✔️
+understanding margin padding -margin meaning distance between two elements and padding means in side .✔️
+understanding backgroung image and things realted to it ✔️
+understanding position absolute ✔️ absolute make different plans and can move anywhere if want to make in move in same container use relative for the parent element in positon we have different element position relative,absolute,static(default),sticky,fixed
+understanding flexbox ✔️🎉
+flexbox 
+make (display: flex;) in parent class
+remeber two things
+main axis justify content: value
+cross axis align items:value
